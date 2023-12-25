ff946d95e00418accdb1f2a55701815129dde0c2
Jake Gillow <jake.gillow@gmail.com>
2023-12-24 20:15:30 -0700

    window and base load of ship done

---
+++ alien_invasion.py
@@ -0,0 +1,43 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion: 
+    #Overall class to manage game assest and behavior.
+    def __init__(self) -> None:
+        #initiates the game and creates game resources
+        pygame.init()
+        self.clock : pygame.time.Clock = pygame.time.Clock()
+        self.settings : Settings = Settings()
+
+        self.screen : pygame.display = pygame.display.set_mode((self.settings.screenWidth,self.settings.screenHeight))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship : Ship = Ship(self)
+
+    def run_game(self) -> None:
+        #starts the main loop
+        while True:
+            #waits for keyboard activity
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #actually set the background color each loop
+            self.screen.fill(self.settings.backgroundColor)
+
+            #create a visual of the ship
+            self.ship.blitme()
+            
+            #makes the most recent screen visible
+            pygame.display.flip()
+
+            #set the clock rate, unit: frames per second
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    #creates an instance of the game and runs it. 
+    ai : AlienInvasion = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    #store all of the settings for knock off space invaders game
+    def __init__(self) -> None:
+        #intialize all of the game settings
+         
+        #Screen settings
+        self.screenWidth = 1200 #pixels
+        self.screenHeight = 800 #pixels
+        self.backgroundColor = (230,230,230)
